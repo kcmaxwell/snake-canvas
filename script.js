8e3c2a81e9c0b2efe5e584a7df9a785a4733cea0
Kristopher Maxwell <zerotrig@gmail.com>
2021-11-30 15:04:49 -0700

    Canvas test

---
+++ script.js
@@ -0,0 +1,8 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.rect(20, 40, 50, 50);
+ctx.fillStyle = "blue";
+ctx.fill();
+ctx.closePath();
