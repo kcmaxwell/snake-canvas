4a8d50d2101f4e8cc01a7d062d27cad3fec6e6aa
Kristopher Maxwell <zerotrig@gmail.com>
2021-11-30 15:50:03 -0700

    Testing drawing fruit in the middle of a tile

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
-var fruitX = 10;
-var fruitY = 10;
+// sizes for the various objects in the game
+var tileLength = 16;
+var fruitRadius = 10; // should be less than tileLength
+
+// the fruit's x and y position in the tile system
+var fruitX = 5;
+var fruitY = 3;
 
 function draw() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -12,7 +17,7 @@ function draw() {
 
 function drawFruit() {
 	ctx.beginPath();
-	ctx.arc(fruitX, fruitY, 10, 0, Math.PI * 2);
+	ctx.arc((fruitX * tileLength) + (tileLength / 2), (fruitY * tileLength) + (tileLength / 2), fruitRadius, 0, Math.PI * 2);
 	ctx.fillStyle = "white";
 	ctx.fill();
 	ctx.closePath();
@@ -22,4 +27,10 @@ function drawSnake() {
 	
 }
 
+function resizeCanvas() {
+	canvas.width = tileLength * 50;
+	canvas.height = tileLength * 50;
+}
+
+resizeCanvas();
 setInterval(draw, 16.6667);
